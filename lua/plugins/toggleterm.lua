8ea6025271a562d8beade0286d6a93509547780a
matthew-mcc <matthew.mcconnell1@ucalgary.ca>
2026-02-04 22:01:51 -0700

    Modified terminal behavior

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -11,7 +11,7 @@ return {
       start_in_insert = true,
       insert_mappings = true,
       persist_size = true,
-      direction = 'horizontal', -- 'vertical' | 'horizontal' | 'window' | 'float'
+      direction = 'float', -- 'vertical' | 'horizontal' | 'window' | 'float'
       close_on_exit = true,
       shell = "fish",
       float_opts = {
