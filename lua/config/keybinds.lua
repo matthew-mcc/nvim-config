015bef565ddec9ad9c7edcb5b7f446629ce4f8e6
matthew-mcc <matthew.mcconnell1@ucalgary.ca>
2026-02-11 15:10:35 -0700

    Added gl as error definition keybind

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -13,6 +13,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K',  vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 
+    vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     -- Open a terminal at the bottom with 10 lines of height
     vim.keymap.set('n', '<leader>tt', ':botright split | term<CR>a', { desc = 'Open Terminal' })
